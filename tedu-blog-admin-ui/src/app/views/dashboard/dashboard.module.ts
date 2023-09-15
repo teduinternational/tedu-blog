d6b078a595052ee7baefb4c78945c740c117300e
Toan bach <ngoctoan@live.com>
2023-09-15 14:26:51 +0700

    3.3 Complete template

--- tedu-blog-admin-ui/src/app/views/dashboard/dashboard.module.ts
+++ tedu-blog-admin-ui/src/app/views/dashboard/dashboard.module.ts
@@ -12,13 +12,15 @@ import {
   NavModule,
   ProgressModule,
   TableModule,
-  TabsModule
+  TabsModule,
 } from '@coreui/angular';
 import { IconModule } from '@coreui/icons-angular';
 import { ChartjsModule } from '@coreui/angular-chartjs';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+
+import { WidgetsModule } from '../widgets/widgets.module';
 
 @NgModule({
   imports: [
@@ -37,10 +39,9 @@ import { DashboardComponent } from './dashboard.component';
     ButtonGroupModule,
     ChartjsModule,
     AvatarModule,
-    TableModule
-    
+    TableModule,
+    WidgetsModule,
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
 })
-export class DashboardModule {
-}
+export class DashboardModule {}

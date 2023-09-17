795219d7d77510826ca14521971b2242a977e221
Toan bach <ngoctoan@live.com>
2023-09-17 17:22:21 +0700

    3.9. Logout and loading

--- tedu-blog-admin-ui/src/app/containers/default-layout/default-header/default-header.component.ts
+++ tedu-blog-admin-ui/src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
+import { UrlConstants } from 'src/app/shared/constants/url.constants';
+import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
 
 @Component({
   selector: 'app-default-header',
@@ -15,7 +18,15 @@ export class DefaultHeaderComponent extends HeaderComponent {
   public newTasks = new Array(5)
   public newNotifications = new Array(5)
 
-  constructor(private classToggler: ClassToggleService) {
+  constructor(private classToggler: ClassToggleService,
+    private tokenService: TokenStorageService,
+    private router: Router) {
     super();
+  }
+
+  
+  logout() {
+    this.tokenService.signOut();
+    this.router.navigate([UrlConstants.LOGIN]);
   }
 }
--- tedu-blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ tedu-blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -14,13 +14,16 @@ import {
 import { AlertService } from 'src/app/shared/services/alert.service';
 import { UrlConstants } from 'src/app/shared/constants/url.constants';
 import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy{
   loginForm: FormGroup;
+  private ngUnsubscribe = new Subject<void>();
+  loading = false;
 
   constructor(
     private fb: FormBuilder,
@@ -34,14 +37,22 @@ export class LoginComponent {
       password: new FormControl('', Validators.required),
     });
   }
+  
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
   login() {
+    this.loading = true;
     var request: LoginRequest = new LoginRequest({
       userName: this.loginForm.controls['userName'].value,
       password: this.loginForm.controls['password'].value,
     });
 
-    this.authApiClient.login(request).subscribe({
+    this.authApiClient.login(request)
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe({
       next: (res: AuthenticatedResult) => {
         //Save token and refresh token to localstorage
         this.tokenSerivce.saveToken(res.token);
@@ -53,6 +64,7 @@ export class LoginComponent {
       error: (error: any) => {
         console.log(error);
         this.alertService.showError('Đăng nhập không đúng.');
+        this.loading = false;
       },
     });
   }

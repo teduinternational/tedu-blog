3640f86ca3789d1e7b1ec0cbd3420bdbffddc500
Toan bach <ngoctoan@live.com>
2023-09-21 10:09:39 +0700

    3.12. role management backend

---
+++ src/TeduBlog.Api/Controllers/AdminApi/RoleController.cs
@@ -0,0 +1,117 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeduBlog.Api.Filters;
+using TeduBlog.Core.Domain.Identity;
+using TeduBlog.Core.Models;
+using TeduBlog.Core.Models.System;
+using TeduBlog.Core.SeedWorks.Constants;
+
+namespace TeduBlog.Api.Controllers.AdminApi
+{
+    [Route("api/admin/role")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IMapper _mapper;
+        public RoleController(RoleManager<AppRole> roleManager, IMapper mapper)
+        {
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [ValidateModel]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<IActionResult> CreateRole([FromBody] CreateUpdateRoleRequest request)
+        {
+            await _roleManager.CreateAsync(new AppRole()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                DisplayName = request.DisplayName,
+            });
+            return new OkResult();
+        }
+
+        [HttpPut("{id}")]
+        [ValidateModel]
+        [Authorize(Permissions.Roles.Edit)]
+        public async Task<IActionResult> UpdateRole(Guid id, [FromBody] CreateUpdateRoleRequest request)
+        {
+            var role = await _roleManager.FindByIdAsync(id.ToString());
+            if (role == null)
+                return NotFound();
+            role.Name = request.Name;
+            role.DisplayName = request.DisplayName;
+
+            await _roleManager.UpdateAsync(role);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Authorize(Permissions.Roles.Delete)]
+        public async Task<IActionResult> DeleteRoles([FromQuery] Guid[] ids)
+        {
+            foreach (var id in ids)
+            {
+                var role = await _roleManager.FindByIdAsync(id.ToString());
+                if (role == null)
+                    return NotFound();
+                await _roleManager.DeleteAsync(role);
+            }
+
+            return Ok();
+        }
+
+
+        [HttpGet("{id}")]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<ActionResult<RoleDto>> GetRoleById(Guid id)
+        {
+            var role = await _roleManager.FindByIdAsync(id.ToString());
+            if (role == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<AppRole, RoleDto>(role));
+        }
+
+        [HttpGet]
+        [Route("paging")]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<ActionResult<PagedResult<RoleDto>>> GetRolesAllPaging(string? keyword, int pageIndex = 1, int pageSize = 10)
+        {
+            var query = _roleManager.Roles;
+            if (!string.IsNullOrEmpty(keyword))
+                query = query.Where(x => x.Name.Contains(keyword)
+                                         || x.DisplayName.Contains(keyword));
+
+            var totalRow = query.Count();
+            query = query.Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize);
+
+            var data = await _mapper.ProjectTo<RoleDto>(query).ToListAsync();
+            var paginationSet = new PagedResult<RoleDto>
+            {
+                Results = data,
+                CurrentPage = pageIndex,
+                RowCount = totalRow,
+                PageSize = pageSize
+            };
+
+            return Ok(paginationSet);
+        }
+
+        [HttpGet("all")]
+        [Authorize(Permissions.Roles.View)]
+        public async Task<ActionResult<List<RoleDto>>> GetAllRoles()
+        {
+            var model = await _mapper.ProjectTo<RoleDto>(_roleManager.Roles).ToListAsync();
+            return Ok(model);
+        }
+    }
+}
---
+++ src/TeduBlog.Core/Models/System/RoleDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduBlog.Core.Domain.Identity;
+
+namespace TeduBlog.Core.Models.System
+{
+    public class RoleDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public class AutoMapperProfiles : Profile
+        {
+            public AutoMapperProfiles()
+            {
+                CreateMap<AppRole, RoleDto>();
+            }
+        }
+    }
+}

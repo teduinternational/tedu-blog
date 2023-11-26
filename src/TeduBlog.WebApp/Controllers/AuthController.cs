5d929da53ec1e3db08c560749f0e952ffc15e52a
Toan bach <ngoctoan@live.com>
2023-11-27 06:50:49 +0700

    4.10. Regsiter new user

---
+++ src/TeduBlog.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TeduBlog.Core.Domain.Identity;
+using TeduBlog.Core.SeedWorks.Constants;
+using TeduBlog.WebApp.Models;
+
+namespace TeduBlog.WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AuthController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [Route("/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromForm] RegsiterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var result = await _userManager.CreateAsync(new AppUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                UserName = model.Email,
+            }, model.Password);
+
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                await _signInManager.SignInAsync(user, true);
+                return Redirect(UrlConsts.Profile);
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
--- src/TeduBlog.WebApp/Program.cs
+++ src/TeduBlog.WebApp/Program.cs
@@ -10,6 +10,7 @@ using TeduBlog.Data.SeedWorks;
 using TeduBlog.WebApp.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var configuration = builder.Configuration;
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -18,12 +19,12 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnCh
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
 //Custom setup
 builder.Services.Configure<SystemConfig>(configuration.GetSection("SystemConfig"));
 
 builder.Services.AddDbContext<TeduBlogContext>(options => options.UseSqlServer(connectionString));
 
+#region Configure Identity
 builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<TeduBlogContext>()
                   .AddDefaultTokenProviders();
@@ -49,16 +50,20 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.User.RequireUniqueEmail = true;
 });
 
-// Add services to the container.
-builder.Services.AddScoped(typeof(IRepository<,>), typeof(RepositoryBase<,>));
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-builder.Services.AddAutoMapper(typeof(PostInListDto));
-
 builder.Services.AddScoped<SignInManager<AppUser>, SignInManager<AppUser>>();
 builder.Services.AddScoped<UserManager<AppUser>, UserManager<AppUser>>();
 builder.Services.AddScoped<RoleManager<AppRole>, RoleManager<AppRole>>();
 
+builder.Services.AddScoped<IUserClaimsPrincipalFactory<AppUser>,
+   CustomClaimsPrincipalFactory>();
+#endregion
+
+builder.Services.AddAutoMapper(typeof(PostInListDto));
+
+#region Configure Services
+// Add services to the container.
+builder.Services.AddScoped(typeof(IRepository<,>), typeof(RepositoryBase<,>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Business services and repositories
 var services = typeof(PostRepository).Assembly.GetTypes()
     .Where(x => x.GetInterfaces().Any(i => i.Name == typeof(IRepository<,>).Name)
@@ -74,9 +79,7 @@ foreach (var service in services)
     }
 }
 
-
-builder.Services.AddScoped<IUserClaimsPrincipalFactory<AppUser>,
-   CustomClaimsPrincipalFactory>();
+#endregion
 
 //Start pipeline
 var app = builder.Build();
